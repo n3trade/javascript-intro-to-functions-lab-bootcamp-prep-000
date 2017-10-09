ef7a10aa400400dcc3c8b5224022fc1564c3f324
Ian Pavlovic <ian_pavlovic@msn.com>
2017-10-09 14:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return ("I can\'t hear you!")
   }
-  else if (string.toUpperCase === string) {
+  else if (string === string.toUpperCase) {
     return ("YES INDEED!")
   }
 }
