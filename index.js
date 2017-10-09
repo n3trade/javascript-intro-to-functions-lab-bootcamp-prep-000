3b58e730f6582b9fddb1bfb2315fc25d25a17bf0
Ian Pavlovic <ian_pavlovic@msn.com>
2017-10-09 14:46:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string === string.toLowerCase) {
     return ("I can\'t hear you!")
   }
 }
